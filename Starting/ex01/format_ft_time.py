30e50ab63243b3123435a5f80a04955416c9b765
eablak <esrablk9@gmail.com>
2024-10-05 15:07:32 +0300

    x

--- Starting/ex01/format_ft_time.py
+++ Starting/ex01/format_ft_time.py
@@ -2,4 +2,5 @@ import datetime
 
 now = datetime.datetime.now()
 print(f"Seconds since January 1, 1970: {now.timestamp():,.4f} or {now.timestamp():.2e} in scientific notation")
-print(now.strftime("%b %d %Y"))  
+print(now.strftime("%b %d %Y"))
+print("aasd")
