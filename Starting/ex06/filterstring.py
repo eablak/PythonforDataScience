d9c6445929ca991b16d30f96eb2bd6f00d28eaed
eablak <esrablk9@gmail.com>
2025-01-11 20:28:46 +0300

    flake8

--- Starting/ex06/filterstring.py
+++ Starting/ex06/filterstring.py
@@ -1,6 +1,7 @@
 import sys
 from ft_filter import ft_filter
 
+
 def main():
     args = sys.argv
 
@@ -11,18 +12,18 @@ def main():
             text = args[1]
             n = int(args[2])
 
-            if not isinstance(text,str) or not isinstance(n, int):
+            if not isinstance(text, str) or not isinstance(n, int):
                 raise AssertionError("Incompatible argument")
             if any(not word.isalpha() for word in text.split()):
-                raise AssertionError ("Bad argument for string")
-            
+                raise AssertionError("Bad argument for string")
             result = list(ft_filter(lambda text: len(text) > n, text.split()))
             print(result)
 
     except ValueError as v:
-        print(ValueError.__name__, ":", v)  
+        print(ValueError.__name__, ":", v)
     except AssertionError as e:
         print(AssertionError.__name__, ":", e)
+
 
 if __name__ == "__main__":
     main()
