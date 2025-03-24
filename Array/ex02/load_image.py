83dbf920fb6c761ee46a6a5b5cbae2c9091eca07
eablak <esrablk9@gmail.com>
2025-03-24 06:26:51 +0300

    ex02 ok

---
+++ Array/ex02/load_image.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import os
+
+def ft_load(path: str) -> np.array:
+
+    try:
+        if not os.path.exists(path):
+            raise AssertionError("File not found")
+        if not path.lower().endswith((".jpeg",".jpg")):
+            raise AssertionError("Only jpeg or jpg supported")
+        img = cv2.imread(path) # read as a blue gran red
+        print(f"The shape of image is: {img.shape}")
+        rgb_format = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert to rgb
+        return(np.array(rgb_format))
+    except AssertionError as e:
+        print(e)
+        return ""
